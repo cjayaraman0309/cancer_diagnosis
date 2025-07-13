ccf826cfe424c888352c71bb1f51318f2269c84a
cjayaraman0309 <chandrasekar.jey@gmail.com>
2025-07-13 15:20:03 +0000

    Add model training script

--- src/train.py
+++ src/train.py
@@ -1,18 +1,18 @@
-import pandas as pd
+with open("src/train.py", "w") as f:
+    f.write('''import pandas as pd
 import mlflow
 import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-# Load data
 df = pd.read_csv('data/preprocess/cancer_data_preprocessed.csv')
 X = df.drop('diagnosis', axis=1)
 y = df['diagnosis']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
 
-with mlflow.start_run():
+with mlflow.start_run() as run:
     clf = RandomForestClassifier(n_estimators=100, random_state=42)
     clf.fit(X_train, y_train)
     preds = clf.predict(X_test)
@@ -20,3 +20,5 @@ with mlflow.start_run():
     mlflow.log_metric("accuracy", acc)
     mlflow.sklearn.log_model(clf, "rf-model")
     print("Accuracy:", acc)
+    print("Run ID:", run.info.run_id)
+''')
