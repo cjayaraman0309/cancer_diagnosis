1a1229d8096912d87a95ce6a36a25c98d67bcf6e
cjayaraman0309 <chandrasekar.jey@gmail.com>
2025-07-13 15:58:51 +0000

    Add model training script with F1 score

--- src/train.py
+++ src/train.py
@@ -1,9 +1,10 @@
-import pandas as pd
+with open("src/train.py", "w") as f:
+    f.write('''import pandas as pd
 import mlflow
 import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, f1_score
 
 df = pd.read_csv('data/preprocess/cancer_data_preprocessed.csv')
 X = df.drop('diagnosis', axis=1)
@@ -16,7 +17,11 @@ with mlflow.start_run() as run:
     clf.fit(X_train, y_train)
     preds = clf.predict(X_test)
     acc = accuracy_score(y_test, preds)
+    f1 = f1_score(y_test, preds)
     mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("f1_score", f1)
     mlflow.sklearn.log_model(clf, "rf-model")
     print("Accuracy:", acc)
+    print("F1 Score:", f1)
     print("Run ID:", run.info.run_id)
+''')
